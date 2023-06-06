84891967594c5b11c5342c6348c56b13b8bb16fe
colleendunlap <colleen85052@gmail.com>
2023-06-06 02:21:10 -0400

    migrated Pokemon and Trainer models

--- fantasy/models.py
+++ fantasy/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Trainer(models.Model):
+    name = models.CharField(max_length=25)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class Pokemon(models.Model):
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 25)
+    species_id = models.IntegerField()
+    species_name = models.CharField(max_length=50)
+    level = models.IntegerField(
+            default=1, 
+            validators=[
+                MaxValueValidator(100),
+                MinValueValidator(1)
+                ]
+            )
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now=True)
