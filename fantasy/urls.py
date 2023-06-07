0456391010b4e418c709356aa6eb64d9f1c55829
colleendunlap <colleen85052@gmail.com>
2023-06-06 22:18:44 -0400

    get team pokemon list endpoint

--- fantasy/urls.py
+++ fantasy/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
         path("", views.index, name = "index"),
+        path("team/<int:team_id>/",views.team_details, name = "team_details"),
         ]
 
 
--- fantasy/views.py
+++ fantasy/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Trainer
+from django.core import serializers
 
 # Create your views here
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello and welcome to Pokemon Fantasy index")
+
+def team_details(request, team_id):
+    team = Trainer.objects.get(id = team_id)
+    pokemon_to_display = team.pokemon_set.all()
+    data = serializers.serialize('json',list(pokemon_to_display))
+    return HttpResponse(data)
