44c75ed377caf3a88891381edcf83cba8ba3d035
colleendunlap <colleen85052@gmail.com>
2023-06-09 10:37:22 -0400

    Trainer model

--- fantasy/models.py
+++ fantasy/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from multiselectfield import MultiSelectField
 
+
+TRADE_STATUSES = ((1, 'pending'),
+            (2, 'invalid'),
+            (3, 'rejected'),
+            (4, 'fulfilled'))
 # Create your models here.
 class Trainer(models.Model):
     name = models.CharField(max_length=25)
@@ -29,4 +35,12 @@ class Pokemon(models.Model):
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
-        return self.name + ' level:' +str(self.level) 
+        return self.name + ' level:' +str(self.level)
+
+class Trade(models.Model):
+    
+    trainer_1 = models.ForeignKey(Trainer, on_delete=models.CASCADE, related_name = 'trainer_1')
+    trainer_2 = models.ForeignKey(Trainer, on_delete=models.CASCADE, related_name = 'trainer_2')
+    pokemon_1 = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name = 'pokemon_1')
+    pokemon_2 = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name = 'pokemon_2')
+    status = MultiSelectField(choices=TRADE_STATUSES, max_choices=1, max_length=1)
